187ecc93e53307b290a5b3a51296ab1f9450882b
Clarisse-3924 <cuwizeyimana3924@gmail.com>
2020-09-26 08:28:45 +0200

    validating day, month, and year in javascript

--- js/index.js
+++ js/index.js
@@ -2,7 +2,21 @@ function result(){
   var d= document.getElementById("day").value;
   var m = document.getElementById("month").value;
   var y = document.getElementById("year").value;
-  if(d==0){
-    alert("days start from 1 to 7");
+  var gender=document.querySelector('input["name="gender"]:checked').value;
+
+  var days = ["monday","tuesday","wesday","thursday","friday","surtday","sunday"];
+var gender=
+
+
+
+
+  if(d <= 0 && d>31){
+    alert("days must be not be less than or equal to zero and it doesn't to be greater than 31");
+  }
+  if(m<=0 && m>12){
+    alert("months have to start from one upto 12");
+  }
+  if(y>2020){
+    alert("You can't born in future")
   }
 }
