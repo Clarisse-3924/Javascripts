4f1dffb6ea68fd8daa3167dc19865e00541dfc9b
Clarisse-3924 <cuwizeyimana3924@gmail.com>
2020-09-26 16:12:59 +0200

    calculation the days of the week

--- js/index.js
+++ js/index.js
@@ -3,75 +3,33 @@ function result(){
   var m = document.getElementById("month").value;
   var y = document.getElementById("year").value;
   var c=document.getElementById("century").value;
-
-  if(d <= 0 && d>31){
-    alert("days must be not be less than or equal to zero and it doesn't to be greater than 31");
-  }
-  if(m<=0 && m>12){
-    alert("months have to start from one upto 12");
-  }
-  if(y>2020){
-    alert("You can't born in future")
-  }
-  var gender=document.querySelector('input["name="gender"]:checked').value;
-  var days= parseInt( ( (c/4) -2*c-1) + ((5*y/4) ) + ((26*(m+1)/10)) + d)%7;
+  var gender = document.querySelector('input[name="gender"]:checked').value;
   var maleNames=[" Kwasi","Kwadwo","Kwabena","Kwaku","Yaw"," Kofi","Kwame"];
 var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var dayw=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sartuday"];
 
-  if(gender==="male"){
-    if(days=== days[0]){
-      console.log("Your Akan name is" +maleNames[0]);
-    }
-    else if(days=== days[1]){
-      console.log("Your Akan name is" +maleNames[1]);
-    }
-    else if(days=== days[2]){
-      console.log("Your Akan name is " +maleNames[2]);
-    }
-    else if(days===days[3]){
-      console.log("Your Akan name is " +maleNames[3]);
-    }
-    else if(days===days[4]){
-      console.log("Your Akan name is" +maleNames[4]);
-    }
-    else if(days===days[5]){
-      console.log("Your Akan name is " +maleNames[5]);
-    }
-    else if(days===days[6]){
-      console.log("Your Akan name is" +maleNames[6]);
-    }
-
-
+  if(d <=0 || m<=0 ){
+    alert("days must be not be less than or equal to zero ");
   }
+  else if(d>31 || m>12){
+    alert(" it doesn't to be greater than 31");
+  }
+  else if(y>2020){
+    alert("You can't born in future");}
+    else if(d==2 || gender==male){
+alert("you was born on" +dayw[2])
+alert("your akan name is " +maleNames[2])
+    }
+    else if(d==1 || gender==male){
+      alert("you was born" +dayw[0])
+      alert("your akan name is" +maleNames[0])
+    }
 
+  else if(d==1,8 ,15,22,29 || gender==female){
+ alert("Your name is" +femaleNames[0]);
+  }
+  
   else{
-    if(days===days[0]){
-      console.log("Your Akan name is " +" "+femaleNames[0]);
-    }
-    else if(days===days[1]){
-      console.log("Your Akan name is " +" "+femaleNames[1]);
-
-    }
-    else if(days===days[2]){
-      console.log("Your Akan name is " +" "+femaleNames[2]);
-
-    }
-    else if(days===days[3]){
-      console.log("Your Akan name is " +" "+femaleNames[3]);
-
-    }
-    else if(days===days[4]){
-      console.log("Your Akan name is " +" "+femaleNames[4]);
-
-    }
-    else if(days===days[5]){
-      console.log("Your Akan name is " +" "+femaleNames[5]);
-
-    }
-    else if(days===days[6]){
-      console.log("Your Akan name is " +" "+femaleNames[6]);
-
-    }
-
+    return;
   }
 }
